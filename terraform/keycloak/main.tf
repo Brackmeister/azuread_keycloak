57fb85a14e53fe14fae642d1e1e4d64b23f30d3c
Brackmeister <brackmeister@gmx.de>
2023-03-03 16:28:13 +0100

    Change frontend client to partial scope with just broker role

--- terraform/keycloak/main.tf
+++ terraform/keycloak/main.tf
@@ -32,10 +32,22 @@ resource "keycloak_saml_identity_provider" "azure_ad" {
   force_authn                   = true
 }
 
+data "keycloak_openid_client" "broker" {
+  realm_id  = keycloak_realm.realm.id
+  client_id = "broker"
+}
+
+data "keycloak_role" "broker__read_token" {
+  realm_id    = keycloak_realm.realm.id
+  client_id   = data.keycloak_openid_client.broker.id
+  name        = "read-token"
+}
+
 resource "keycloak_openid_client" "frontend" {
   realm_id  = keycloak_realm.realm.id
   client_id = "frontend"
 
+  full_scope_allowed    = false
   enabled               = true
   access_type           = "PUBLIC"
   standard_flow_enabled = true
@@ -43,5 +55,10 @@ resource "keycloak_openid_client" "frontend" {
     "https://oauth.pstmn.io/v1/callback",
     "${var.keycloak_baseurl}/realms/${keycloak_realm.realm.realm}/*"
   ]
-  login_theme = "keycloak"
+}
+
+resource "keycloak_generic_role_mapper" "broker_to_frontend_role_mapper" {
+  realm_id  = keycloak_realm.realm.id
+  client_id = keycloak_openid_client.frontend.id
+  role_id   = data.keycloak_role.broker__read_token.id
 }
